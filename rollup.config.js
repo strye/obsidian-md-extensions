5e2ff1b3c513d3a29d24dd4e49308daeafd9f2b4
Will Strye <will.strye@malignantgenius.com>
2022-07-31 17:43:12 -0700

    Updated Styles

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,20 @@
 // rollup --config
+import copy from 'rollup-plugin-copy'
+
 export default {
 	input: './src/plugin.js',
   
 	output: [
 		{ // required (can be an array, for multiple outputs)
-			file: './main.js',
+			file: './dist/main.js',
 			format: 'cjs', // required
 		},
+	],
+	plugins: [
+		copy({
+			targets: [
+				{ src: './manifest.json', dest: './dist' }
+			]
+		})
 	]
 };
